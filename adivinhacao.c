dc82ff30eb9ec11b57b7f3289a677ec0b62ee1ed
Herbert174 <herbertms174@gmail.com>
2026-05-10 17:26:28 -0300

    Subindo pasta de estudos pro git

---
+++ adivinhacao.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("**********************************************\n");
+    printf("* Bem vindo ao nosso jogo de adivinhação 2.0 *\n");
+    printf("**********************************************\n");
+}
+
