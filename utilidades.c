ba9c2d6e8b14be94b122d00cc1862b79641894da
Herbert174 <herbertms174@gmail.com>
2026-05-10 17:44:57 -0300

    Att info uteis

---
+++ utilidades.c
@@ -0,0 +1,25 @@
+//     Comandos terminais uteis para trabalhar com C e C++
+
+// ls  -  Lista os arquivos dentro da pasta atual
+// pwd -  Mostra o endereço da pasta atual
+// clear - Limpa o cmd
+// cd (Pasta) - Entra na pasta solicitada
+// cd .. - Volta para a pasta anterior
+// cat (Arquivo) - Imprime arquivo apontado (Ex. cat programa.c )
+// rm (Arquivo) - Apaga o arquivo apontado (Ex. rm programa.exe)
+// mv (Arquivo) - Renomeia o arquivo apontado para o novo nome (Ex. programa.c adivinhacao.c)
+/* vi (Arquivo) - Visualiza o arquivo (Dentro da visualização, apertando a tecla i podemos 
+entrar mo modo de edição e alterar via monitor as informação do arquivo)
+                - Também é possivel criar novos arquivos ao mandar o comando vi (nome novo arquivo) 
+                - Apertando Esc e : podemos ir para o modo de comandos do vi 
+                - Comandos uteis no vi
+                      q! (Sai sem salvar qualquer alteração feita)
+                      wq! (Sai e salva qualquer alteração realizada no arquivo)
+                      !rm % (Exclui o arquivo aberto, após isso caso o mesmo esteja em visualização
+                            mande o comando q! utilize o ls para confirmar se o mesmo foi deletado)*/
+
+
+/* gcc (Arquivo) -o (Nome do Arquivo compilado a ser gerado ou alterado) 
+                - Compila o script C e C++ e   (Ex. gcc programa.c -0 programa.exe )         */
+
+// ./ (Arquivo) manda rodar o arquivo  (Ex. ./programa.exe )
