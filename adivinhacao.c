96ccf69d933cd0ff8060d0e4c5de73b1ad416d96
HerbertWPS <herbert.santos@wps-sa.com.br>
2026-05-14 09:32:03 -0300

    Primeiro commit teste

--- adivinhacao.c
+++ adivinhacao.c
@@ -5,5 +5,6 @@ int main()
     printf("**********************************************\n");
     printf("* Bem vindo ao nosso jogo de adivinhação 2.0 *\n");
     printf("**********************************************\n");
+    printf("Primeira alteracao aqui");
 }
 
