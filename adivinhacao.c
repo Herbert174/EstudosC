7790980449cd296faef364135df88d136a1cfc3c
HerbertWPS <herbert.santos@wps-sa.com.br>
2026-05-18 16:12:14 -0300

    For e if licao

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define NUMERO_TENTATIVAS 5
+
 int main()
 {
     printf("**********************************************\n");
@@ -10,31 +12,45 @@ int main()
     /*int idade;
     idade = 26;*/
     int palpite;
-
-    printf("Qual e o seu palpite? ");
-    scanf("%d", &palpite);  //%d mascara para recuperar valores inteiros
-    printf("Seu palpite foi %d \n", palpite);
     
-    int acertou = palpite == numerosecreto;
-    printf("Acerto: %d\n", acertou);
 
-    //printf("Voce errou, o numero secreto era %d \n", numerosecreto);
-    //printf("O numero secreto e o %d nao conta para ninguem\n", numerosecreto);
-
-    if(acertou)
+    for(int i = 1; i <= NUMERO_TENTATIVAS; i++)
         {
-        printf("Parabens voce acertou!\n");
-        printf("Jogue novamente meu caro player\n");
-        }else{
-            printf("Voce errou\n");
-            if(palpite < numerosecreto)
-                {
-                printf("O seu palpite e menor que o numero secreto\n");
-                }
-            if(palpite > numerosecreto)
-                {
-                printf("O seu palpite e maior que o numero secreto\n");
-                }
+        printf("Tentativa %d / %d \n", i, NUMERO_TENTATIVAS);
+        printf("Qual e o seu palpite? ");
+        scanf("%d", &palpite);  //%d mascara para recuperar valores inteiros
+        printf("Seu palpite foi %d \n", palpite);
+
+        if(palpite < 0)
+            {
+            printf("Voce nao pode chutar numeros negativos!\n\n");
+            i--;
+            continue; //Encerrar direto o loop (for) e ir direto para o i++
             }
-    
+        
+        int acertou = palpite == numerosecreto;
+        int maior = palpite > numerosecreto;
+        int menor = palpite < numerosecreto;
+        //printf("Acerto: %d\n", acertou);
+
+        //printf("Voce errou, o numero secreto era %d \n", numerosecreto);
+        //printf("O numero secreto e o %d nao conta para ninguem\n", numerosecreto);
+
+        if(acertou)
+            {
+            printf("Parabens voce acertou!\n\n");
+            break;
+            }else{
+                printf("Voce errou\n");
+                if(maior)
+                    {
+                    printf("O seu palpite e maior que o numero secreto\n\n");
+                    }
+                else if(menor)
+                    {
+                    printf("O seu palpite e menor que o numero secreto\n\n");
+                    }
+                }
+        }
+    printf("Fim de jogo \n");
 }
